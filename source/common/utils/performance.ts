1ce77e164bb4ec8de71ab5ed8358380e774025a0
花楹一间 <101607534+Aurora-flower@users.noreply.github.com>
2025-02-08 02:59:44 +0800

    feat(common): 实现防抖函数

--- source/common/utils/performance.ts
+++ source/common/utils/performance.ts
@@ -4,22 +4,20 @@
  */
 
 export class PreformanceUtility {
-  // /**
-  //  * 防抖
-  //  * @param callback 回调函数
-  //  * @param delay 延迟时间
-  //  * @returns
-  //  */
-  // static debounce(
-  //   callback: Function,
-  //   delay: number = 1000
-  // ): Function {
-  //   let timer: number | null = null;
-  //   return (...args: any[]) => {
-  //     if (timer) {
-  //       clearTimeout(timer);
-  //     }
-  //     timer = setTimeout(() => {});
-  //   };
-  // }
+  /**
+   * 防抖
+   * @param callback 回调函数
+   * @param delay 延迟时间
+   * @returns
+   */
+  static debounce(callback: Function, wait: number) {
+    let timeout: NodeJS.Timeout | null = null;
+    return (...args: unknown[]) => {
+      timeout && clearTimeout(timeout);
+      timeout = setTimeout(
+        () => callback.apply(this, args),
+        wait
+      );
+    };
+  }
 }
