121b7362e266fd34ad77eeed1b9b857a187cdfd2
花楹一间 <101607534+Aurora-flower@users.noreply.github.com>
2025-02-20 10:57:06 +0800

    feat(api): 添加媒体查询相关函数并更新样式表
    - 在 device/index.ts 中添加了 matchMedia 和 getMatchDedia 函数，用于监听和获取媒体查询结果
    - 在 media.css 中增加了用于检测用户主题设置和减少动画效果的媒体查询规则

--- source/src/api/web/device/index.ts
+++ source/src/api/web/device/index.ts
@@ -2,3 +2,19 @@
  * @file 存放与设备相关的 web api
  */
 export * from '@/src/api/web/device/navigator';
+
+/**
+ * @summary 监听媒体查询
+ * @param query 媒体查询
+ */
+export function matchMedia(query: string) {
+  return window.matchMedia(query); //.matches;
+}
+
+/**
+ * @summary 获取媒体查询结果
+ * @param media 媒体查询
+ */
+export function getMatchDedia(media: MediaQueryList) {
+  return media.matches;
+}
