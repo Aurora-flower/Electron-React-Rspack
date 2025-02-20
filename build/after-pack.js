5e49bba0eb935c9b284fe138002159850a213457
花楹一间 <101607534+Aurora-flower@users.noreply.github.com>
2025-02-20 22:18:16 +0800

    refactor(build): 重构打包配置并优化 TypeScript 设置
    - 移除 after-pack.js 中的注释代码
    - 重命名 rspack.config.ts 为 gulpfile.js/rspack/rspack.config.js
    - 在 rsbuild-project 和 rspack-project 的 tsconfig.json 中添加 forceConsistentCasingInFileNames 设置

--- build/after-pack.js
+++ build/after-pack.js
@@ -6,8 +6,6 @@
 const { moveDir } = require('./utils/file');
 
 exports.default = function () {
-  // const folder = join(process.cwd(), 'core');
-  // const destFolder = join(process.cwd(), 'app/core');
   moveDir('gen', 'app/gen');
   moveDir('core', 'app/core');
 };
