c825d6f13073913feec1d88bc65535ba98420e86
花楹一间 <101607534+Aurora-flower@users.noreply.github.com>
2025-02-23 01:06:18 +0800

    docs(extend): 添加 Babel 功能文档
    - 新增 Babel 功能文档，介绍 Babel 的语法转换、JSX 转换、Polyfill 和插件预设功能
    - 更新项目配置，添加 Babel 相关依赖和配置文件
    - 重构 gulp 任务，使用 RspackCLI 替代 rspack
    - 移除 log 目录下的旧文件，重新组织日志结构

--- gulpfile.js/tasks/dev.js
+++ gulpfile.js/tasks/dev.js
@@ -3,12 +3,12 @@
  * @description
  * 结合 rspack 与 gulp 构建
  */
-const rspack = require('rspack');
+const { RspackCLI } = require('@rspack/cli');
 const { task } = require('gulp');
 const { getArgv } = require('../utils/argv');
 const get = require('../webpack/get_config');
 
 task('dev', async function () {
   const args = getArgv();
-  console.log('dev task running...', args, rspack);
+  console.log('dev task running...', args, RspackCLI, get());
 });
