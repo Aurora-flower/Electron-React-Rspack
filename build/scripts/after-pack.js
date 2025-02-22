2ea38bc7ffdcb65c43d76dfcb8ad82bea649a6a7
花楹一间 <101607534+Aurora-flower@users.noreply.github.com>
2025-02-23 07:01:37 +0800

    refactor(project): 重构项目结构并更新配置
    - 更新 CHANGELOG.md 文件格式
    - 重命名 build 相关脚本和工具
    - 新增 Rspack 文档
    - 更新 electron-builder 配置
    - 新增资源模块类型配置
    - 重命名 plopfile.js

--- build/scripts/after-pack.js
+++ build/scripts/after-pack.js
@@ -2,7 +2,6 @@
  * @file 在执行完 electron-builder 后的操作
  * @description 现在是只有对 app/core 目录进行移动
  */
-
 const { moveDir } = require('./utils/file');
 
 exports.default = function () {
---
+++ gulpfile.js/common/asset_module.js
@@ -0,0 +1,26 @@
+/**
+ * @summary 资源模块的模块类型
+ * @remarks
+ * - 使用 'asset/inline' 替换 `url-loader`
+ * - 使用 'asset/resource' 替换 `file-loader`
+ * - 使用 'asset/source' 替换 `raw-loader`
+ */
+const AssetModuleType = {
+  /* 将资源转换为单独的文件，并且导出产物地址 */
+  Resource: 'asset/resource',
+
+  /* 将资源转换为 DataURI，使用 Base64 编码，暂不支持编码配置 */
+  Inline: 'asset/inline',
+
+  /* 将资源文件转为字符串导出 */
+  Source: 'asset/source',
+
+  /**
+   * @summary 自动选择资源模块类型
+   * @description
+   * 默认如果资源体积小于等于 8096 bytes(size < 8kb)，则使用 'asset/inline' 策略，否则使用 'asset/resource' 策略。
+   */
+  Auto: 'asset'
+};
+
+module.exports = { AssetModuleType };
