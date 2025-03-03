78a05299349b59336109b00ccf1cc6554bd0fcd8
花楹一间 <101607534+Aurora-flower@users.noreply.github.com>
2025-03-03 10:30:09 +0800

    docs: 添加多篇技术文档和示例代码
    - 新增 EventEmitter 示例，演示 Node.js 中事件处理的 this 绑定
    - 添加 AJAX 技术文档，介绍 XMLHttpRequest 的使用方法
    - 新增 OpenCV 面部识别示例代码
    - 调整 Gulp 调试任务文件位置
    - 更新 Electron IPC 通信文档，增加更多细节说明
    - 新增 React 复合模式和高阶组件模式示例代码
    - 添加 Vanilla JS 单例模式示例代码

--- gulpfile.js/tasks/debug.js
+++ gulpfile.js/tasks/debug.js
@@ -2,11 +2,8 @@
  * @file 用于调试 getConfig 与 compile
  */
 const { task } = require('gulp');
-const getConfig = require('../webpack/get_config');
 
 function debug(cd) {
-  getConfig();
-
   cd();
 }
 
--- source/electron/handler/channels.ts
+++ source/electron/handler/channels.ts
@@ -3,6 +3,15 @@
  * @description
  * 在 Electron 中，IPC 用于主进程（Main Process）与渲染进程（Renderer Process）之间的通信。
  * 由于主进程和渲染进程是两个不同的进程，彼此之间不能直接共享数据，因此需要使用 IPC 来交换信息。
+ *
+ * @remarks
+ * - Electron 提供了 `ipcMain` 和 `ipcRenderer` 两个模块来处理 IPC 通信:
+ *    - `ipcMain`：用于在主进程中监听和响应来自渲染进程的消息。
+ *    - `ipcRenderer`：用于在渲染进程中发送消息到主进程，并接收主进程的响应。
+ *
+ * - `**对象序列化(结构化克隆序列化)**`:
+ *    Electron 的 IPC 实现使用 HTML 标准的 `结构化克隆算法` 来序列化进程之间传递的对象，
+ *    这意味着只有某些类型的对象可以通过 IPC 通道传递。
  */
 
 import debugLog from '@/electron/tools/log';
@@ -11,6 +20,33 @@ import debugLog from '@/electron/tools/log';
 
 const Panel = {};
 
+/**
+ * @summary  启用 IPC 消息渠道 (APP_PIPE_ACTIONS)
+ * @remarks
+ * 1. **渲染進程 -> 主進程**
+ * - 单向：
+ *    - 发送消息：`ipcRenderer.send`
+ *    - 接收消息：`ipcMain.on`
+ *
+ * - 双向：
+ *    - 发送消息：`ipcRenderer.invoke` -- 发送请求并等待响应 -- Promise 返回值
+ *    - 接收消息：`ipcMain.handle`
+ *
+ * - 旧版本异步双向通信的推荐方式：
+ *    - 发送消息: `ipcRenderer.send`（渲染进程发送至主进程） + `ipcRenderer.on`（接收主进程的回复）
+ *    - 接收消息: `ipcMain.on`（接收渲染进程发送的信号） +  `event.reply`（回应渲染进程的信号）
+ *
+ * 2. **主进程 -> 渲染进程**:
+ *    - 发送消息: `Window.webContents.send`
+ *    - 接收消息: `ipcRenderer.on`
+ *
+ * 注意📢：
+ *  - `event.reply` 是 `ipcMain` 模块中的一个方法，用于从主进程 _**渲染进程**_ 向 _**渲染进程**_ 发送响应消息。
+ *  当主进程监听到某个事件后，可以通过 `event.reply`方法来发送数据回给渲染进程。
+ *  - **安全措施**:  _默认验证 所有 IPC 消息 senders_
+ *  - IPC 消息命名规范: IPC 的消息名就是一个字符串，对于消息的发送者，可以自由的进行消息的命名。
+ *  但是希望在定制消息时遵守一定的规范，防止混乱。命名规范为 `module-name:action-name` 或 `package-name:action-name`。
+ */
 export function channelEventDispatch(
   channel: string,
   ...args: unknown[]
