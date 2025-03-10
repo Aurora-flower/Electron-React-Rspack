362a570c21caaed9c054cdddafde3521f6d2e9a1
花楹一间 <101607534+Aurora-flower@users.noreply.github.com>
2025-03-10 18:01:52 +0800

    refactor: 移除未使用的模板和配置文件
    - 删除了多个未使用的模板和配置文件，包括 rsbuild、rspack 项目的相关文件
    - 移除了 temp 目录下的多个临时文件和配置
    - 删除了 source 目录下的未使用布局文件

--- gulpfile.js/tasks/package.js
+++ gulpfile.js/tasks/package.js
@@ -1,5 +1,5 @@
 /**
- * @file 开发运行任务
+ * @file 打包构建应用程序任务
  * @description
  * 结合 rspack、 gulp、electron-builder 构建
  */
--- gulpfile.js/webpack/webpack_target.js
+++ gulpfile.js/webpack/webpack_target.js
@@ -34,7 +34,7 @@ const WebpakTarget = {
 
   /*
   Electron 渲染进程的目标
-  注意📢：
+  注意📢: 
     - 当为 electron-renderer 时，webpack 的配置属性 optimization，使用 splitChunks 则会导致失败；
     - 使用此目标时需注意 nodeIntegration 的设置，否则可能会报错 global is not defined
   */
