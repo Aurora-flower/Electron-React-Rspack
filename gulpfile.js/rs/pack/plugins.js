3fcb3e979feab7fef19f84e5b8ca135d93796527
花楹一间 <yanl_802@163.com>
2025-03-21 14:53:57 +0800

    refactor(dep): 调整 electron-devtools-installer 为生产依赖

--- gulpfile.js/rs/pack/plugins.js
+++ gulpfile.js/rs/pack/plugins.js
@@ -7,8 +7,15 @@ const { rspack } = require('@rspack/core');
  * @summary 获取 HtmlRspackPlugin 实例
  * @param {*} template 模板文件路径
  * @returns {rspack.HtmlRspackPlugin} HtmlRspackPlugin 实例
+ *
  */
 function getHtmlRspackPlugin(template) {
+  /**
+   * @summary
+   * - `style-src`：控制所有 CSS 来源（包括内联样式和外部样式）
+   * - `style-src-attr`：仅控制 HTML 元素内联样式（如 <div style="...">）
+   * - `style-src-elem`：更细粒度地聚焦外部样式表和 <style> 标签内容
+   */
   const HtmlRspackPlugin = new rspack.HtmlRspackPlugin({
     template,
     inject: 'body',
