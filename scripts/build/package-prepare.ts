494626a67e55ae1fc55887858e5297fe03a6cee3
花楹一间 <101607534+Aurora-flower@users.noreply.github.com>
2025-04-24 00:45:30 +0800

    del(scripts): 移除打包准备流程脚本中未使用的代码

--- scripts/build/package-prepare.ts
+++ scripts/build/package-prepare.ts
@@ -1,8 +1,5 @@
-import { type ChildProcess, exec } from "child_process";
-import { createWriteStream, readFileSync } from "fs";
 import { join, relative } from "node:path";
 import { mkdir, readFile, writeFile } from "node:fs/promises";
-// const { rimraf } = require("rimraf");
 
 interface PackageJson {
   name: string;
@@ -13,10 +10,6 @@ interface PackageJson {
   dependencies?: Record<string, string>;
 }
 
-// interface DistPackage extends Omit<PackageJson, "main"> {
-//   main: string;
-// }
-
 type DistPackage = Partial<PackageJson> & {
   main: string;
 };
@@ -24,43 +17,6 @@ type DistPackage = Partial<PackageJson> & {
 const outDir = join(process.cwd(), "app");
 const packagePath = join(process.cwd(), "package.json");
 const outputPath = join(outDir, "package.json");
-
-function execSetup(command: string) {
-  return new Promise((resolve, reject) => {
-    const child: ChildProcess = exec(command);
-    // child.stdout &&
-    //   child.stdout.on("data", (data) => {
-    //     console.log(data.toString());
-    //   });
-    // child.stderr &&
-    //   child.stderr.on("data", (data) => {
-    //     console.log(data.toString());
-    //   });
-    // child.on("close", (code) => {
-    //   resolve(code);
-    //   console.log("close", code);
-    // });
-    child.stdout?.pipe(process.stdout);
-    child.stderr?.pipe(process.stderr);
-    child.on("close", (code) => {
-      code === 0
-        ? resolve(code)
-        : reject(new Error(`Process exited with code ${code}`));
-    });
-  });
-}
-
-function writeFileStream(file: string, data: unknown) {
-  return new Promise((resolve, reject) => {
-    const ws = createWriteStream(file);
-    ws.on("error", reject);
-    ws.on("close", () => {
-      resolve(true);
-    });
-    ws.write(data);
-    ws.end();
-  });
-}
 
 async function preparePackage() {
   try {
