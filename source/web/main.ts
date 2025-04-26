1b74e34d5972a38bbdd133885a510522b43b2139
花楹一间 <101607534+Aurora-flower@users.noreply.github.com>
2025-04-26 15:35:37 +0800

    test: 测试 pixi 渲染功能、进程通信功能、local 协议访问能力

--- source/web/main.ts
+++ source/web/main.ts
@@ -1,7 +1,10 @@
-import { enableWindowMessagesListener } from "@/helpers/event/listener";
+import "pixi.js/unsafe-eval";
 import { sender } from "@/helpers/event/electron";
+import { getRootElement } from "@/helpers/function/dom";
+import { Application, ApplicationOptions, Assets, Sprite } from "pixi.js";
+import { enableWindowMessagesListener } from "@/helpers/event/listener";
 
-document.body.innerHTML = `<div id="app">HELLO WORLD!</div>`;
+const root = "#root";
 
 sender("sms:transmit", {
   channel: "sms:transmit",
@@ -10,7 +13,34 @@ sender("sms:transmit", {
 
 enableWindowMessagesListener((event) => {
   const ev = event as MessageEvent;
-  event.stopImmediatePropagation();
+  event.stopImmediatePropagation(); // 阻止传递
   const origin = ev.origin || location.href;
   console.log("[onMsg]", ev, origin, ev.data, ev.source);
 });
+
+enableWindowMessagesListener((event) => {
+  const ev = event as MessageEvent;
+  event.stopImmediatePropagation();
+  const origin = ev.origin || location.href;
+  console.log("[onMsg 2]", ev, origin, ev.data, ev.source);
+});
+
+(async () => {
+  const app = new Application();
+  const element = getRootElement(root);
+  await app.init({
+    antialias: true,
+    resizeTo: element,
+  } as Partial<ApplicationOptions>);
+  element.appendChild(app.canvas);
+  const url = "local:///Users/HuaYing/Downloads/图片/bg.png"; // electron 自定义协议
+  Assets.load(url).then((texture) => {
+    const sprite = new Sprite({ texture });
+    sprite.anchor.set(0.5);
+    app.stage.addChild(sprite);
+  });
+  // fetch(url).then((res) => {
+  //   console.log("res", res);
+  // });
+  console.log("Root", element, app, url);
+})();
