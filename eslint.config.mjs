7439dd1f6daf51d55922c9fed543d9a05d5527bf
花楹一间 <yanl_802@163.com>
2025-04-28 14:49:52 +0800

    build(deps): 安装 eslint 相关依赖, 初始化 eslint 配置

---
+++ eslint.config.mjs
@@ -0,0 +1,121 @@
+import css from "@eslint/css"
+import js from "@eslint/js"
+import json from "@eslint/json"
+import markdown from "@eslint/markdown"
+import parser from "@typescript-eslint/parser"
+import pluginReact from "eslint-plugin-react"
+import { defineConfig } from "eslint/config"
+import globals from "globals"
+import tseslint from "typescript-eslint"
+
+export default defineConfig([
+  {
+    ignores: [
+      "app/",
+      "dist/",
+      "node_modules/",
+      "temp/",
+      "backup/",
+      "core/",
+      "test/",
+      "release/",
+      "*.config.*",
+      "*config.*",
+      "*config.*.*",
+      "package-lock.json"
+    ]
+  },
+  {
+    files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"],
+    plugins: { js },
+    extends: ["js/recommended"]
+  },
+  {
+    files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"],
+    languageOptions: { globals: { ...globals.browser, ...globals.node } }
+  },
+  tseslint.configs.recommended,
+  pluginReact.configs.flat.recommended,
+  {
+    files: ["**/*.json"],
+    plugins: { json },
+    language: "json/json",
+    extends: ["json/recommended"]
+  },
+  {
+    files: ["**/*.jsonc"],
+    plugins: { json },
+    language: "json/jsonc",
+    extends: ["json/recommended"]
+  },
+  {
+    files: ["**/*.json5"],
+    plugins: { json },
+    language: "json/json5",
+    extends: ["json/recommended"]
+  },
+  {
+    files: ["**/*.md"],
+    plugins: { markdown },
+    language: "markdown/commonmark",
+    extends: ["markdown/recommended"]
+  },
+  {
+    files: ["**/*.css"],
+    plugins: { css },
+    language: "css/css",
+    extends: ["css/recommended"]
+  },
+  {
+    files: ["**/*.ts", "**/*.tsx"],
+    languageOptions: {
+      parser,
+      parserOptions: {
+        ecmaFeatures: { jsx: true }
+      }
+    }
+  },
+  {
+    rules: {
+      "no-multiple-empty-lines": [
+        "error",
+        {
+          max: 1,
+          maxBOF: 0,
+          maxEOF: 0
+        }
+      ],
+      "@typescript-eslint/no-unused-vars": [
+        "error",
+        {
+          "vars": "all",
+          "args": "after-used",
+          "argsIgnorePattern": "^_",
+          "caughtErrors": "all",
+          "caughtErrorsIgnorePattern": "^_",
+          "destructuredArrayIgnorePattern": "^_",
+          "varsIgnorePattern": "^_",
+          "ignoreRestSiblings": true
+        }
+      ],
+      "@typescript-eslint/no-require-imports": "warn",
+      "@typescript-eslint/no-unused-expressions": [
+        "warn",
+        {
+          enforceForJSX: true,
+          // allowShortCircuit: true,
+          allowTernary: true
+        }
+      ],
+      // 与当前 eslint-plugin-react 版本不兼容配置
+      "react/display-name": "off",
+      "react/jsx-key": "off",
+      "react/jsx-uses-react": "off",
+      "react/no-deprecated": "off",
+      "react/no-direct-mutation-state": "off",
+      "react/prop-types": "off",
+      "react/react-in-jsx-scope": "off",
+      "react/require-render-return": "off"
+    }
+  }
+])
