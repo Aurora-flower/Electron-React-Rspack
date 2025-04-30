79316056ec143e7c4ef9f7aeea35cd1aa3af646e
花楹一间 <yanl_802@163.com>
2025-05-01 01:27:34 +0800

    refactor(electron): 优化 SMS 消息处理
    - 在 Message 接口中添加 isJson 字段，用于指示数据是否为 JSON 格式
    - 修改 transmit 函数，根据 isJson 字段决定是否解析数据为 JSON
    - 提高了消息处理的灵活性和准确性

--- source/electron/handlers/channel/message/sms.ts
+++ source/electron/handlers/channel/message/sms.ts
@@ -3,13 +3,16 @@ import WindowManager from "@main/helpers/manager/window"
 interface Message {
   type: string
   data: string
+  isJson: boolean
 }
 
 export async function transmit(msg: Message) {
   const window = WindowManager.getInstance().mainWindow
   if (!window) return
   const message =
-    msg.data && typeof msg.data === "string" ? JSON.parse(msg.data) : msg.data
+    msg.data && typeof msg.data === "string" && msg.isJson
+      ? JSON.parse(msg.data)
+      : msg.data
   window.webContents.send("trigger-message", {
     type: msg.type,
     data: message
