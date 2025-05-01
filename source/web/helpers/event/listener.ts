7ee0598765fb7ed64752c6c6b7fb52bc21350453
花楹一间 <yanl_802@163.com>
2025-05-01 12:08:05 +0800

    refactor(web): 注释未使用的消息监听器代码，关闭事件阻止传播

--- source/web/helpers/event/listener.ts
+++ source/web/helpers/event/listener.ts
@@ -3,15 +3,14 @@ import { enableWindowMessagesListener } from "@/helpers/manager/listener"
 export function messageListener() {
   enableWindowMessagesListener(event => {
     const ev = event as MessageEvent
-    event.stopImmediatePropagation() // 阻止传递
     const origin = ev.origin || location.href
     console.log("[onMsg]", origin, ev.data, ev.source)
   })
 
-  enableWindowMessagesListener(event => {
-    const ev = event as MessageEvent
-    event.stopImmediatePropagation()
-    const origin = ev.origin || location.href
-    console.log("[onMsg 2]", ev, origin, ev.data, ev.source)
-  })
+  // enableWindowMessagesListener(event => {
+  //   const ev = event as MessageEvent
+  //   event.stopImmediatePropagation()
+  //   const origin = ev.origin || location.href
+  //   console.log("[onMsg 2]", ev, origin, ev.data, ev.source)
+  // })
 }
