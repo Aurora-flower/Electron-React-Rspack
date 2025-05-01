232f48978df39c14897cf223fcdbc1f8f611ad03
花楹一间 <yanl_802@163.com>
2025-05-01 12:49:45 +0800

    feat(web): 新增 useClassNames 钩子
    - 实现了一个新的钩子函数 useClassNames，用于处理类名数组
    - 该函数可以过滤掉虚假值，处理字符串、对象和数组类型的类名
    - 最终返回一个合并后的字符串，用于 React 组件的 className 属性
    - 添加了错误捕获和日志记录，提高代码的健壮性

---
+++ source/web/hooks/useClassNames.ts
@@ -0,0 +1,34 @@
+import { DATA_TYPE_MODE, isType } from "@/helpers/function/dataType"
+import { webLog } from "@/helpers/log"
+import CommonUtility from "@/utils/utility"
+
+function useClassNames(classNames: Array<unknown>): string {
+  try {
+    const classnames = classNames.filter(Boolean)
+    const processedClassNames = classnames.reduce(
+      (pre: string[], current, _index, _self) => {
+        if (isType(current, DATA_TYPE_MODE.String)) {
+          const data = current as string
+          pre.push(data.trim() as string)
+        } else if (isType(current, DATA_TYPE_MODE.Object)) {
+          const data = current as ObjectType<unknown>
+          for (const item of Object.keys(data)) {
+            if (item in data && data[item]) {
+              pre.push(item)
+            }
+          }
+        } else if (Array.isArray(current)) {
+          pre.push(...current.filter(Boolean))
+        }
+        return pre
+      },
+      []
+    )
+    return processedClassNames.join(" ").replace(/\bundefined\b/g, "")
+  } catch (error: unknown) {
+    webLog("useClassNames", "error", CommonUtility.errorMessage(error))
+    return ""
+  }
+}
+
+export default useClassNames
