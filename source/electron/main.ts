429d79c9f0ec28498a29680ff41982b0dff629c3
花楹一间 <yanl_802@163.com>
2025-05-06 14:14:17 +0800

    feat(server): 实现 AppServer 类并创建应用本地服务器

--- source/electron/main.ts
+++ source/electron/main.ts
@@ -1,6 +1,7 @@
 import onAppReady from "@main/handlers/event/onAppReady"
 import onAppReadyBefore from "@main/handlers/event/onAppReadyBefore"
 import commandLines from "@main/helpers/function/commandLines"
+import { createAppServer } from "@main/server"
 import Logger from "electron-log"
 
 function advanceExecution() {
@@ -9,5 +10,6 @@ function advanceExecution() {
 ;(function init() {
   advanceExecution()
   onAppReadyBefore().then(onAppReady).catch(console.error)
+  createAppServer()
   Logger.info(process.env.DEV_SERVER_URL, process.env.TITLE)
 })()
--- source/electron/server/index.ts
+++ source/electron/server/index.ts
@@ -1,35 +1,87 @@
-import type http from "node:http"
-import type https from "node:https"
-import Express from "express"
+import * as http from "node:http"
+import * as https from "node:https"
+// import certificate from "@main/private/certificate.pem"
+// import privateKey from "@main/private/private-key.pem"
+import { errorMessage } from "@main/utils/error"
+import Logger from "electron-log"
+import express = require("express")
+import { join } from "node:path"
 
 interface AppServerOptions {
   path: string
   port: number
   hostname: string
-  isSafe: boolean
+  isSafe?: boolean
+  privateKey?: string
+  certificate?: string
 }
 
 export class AppServer {
-  private _option: AppServerOptions | undefined
+  private _options: AppServerOptions | undefined
   private _instance!: AppServer
-  private _application!: Express.Application
+  private _application!: express.Application
   private _server!: http.Server | https.Server
 
   constructor(options?: AppServerOptions) {
     this.option = options
-    this._application = Express()
+    if (options?.path) {
+      this._application = express()
+      const middleware = express.static(options.path)
+      this._application.use(middleware)
+      this._application.get("/", (req, res) => {
+        res.sendFile(join(options.path, "index.html"))
+        // res.send("Hello World")
+      })
+      Logger.log("PATH", join(options.path, "index.html"))
+    }
   }
 
-  start() {}
+  private createHttpServer() {
+    if (!this._application || !this._options) return
+    this._server = http.createServer(this._application)
+    this._server.listen(this._options.port, () => {
+      Logger.log(`HTTP server listening on port ${this._options?.port}`)
+    })
+    this._server.on("error", err => {
+      Logger.log("Server error", errorMessage(err))
+    })
+    Logger.log("createHttpServer", this._options.path)
+  }
 
-  stop() {}
+  private createHttpsServer() {
+    if (!this._application || !this._options) {
+      return
+    }
+    const credentials = {
+      key: this._options.privateKey,
+      cert: this._options.certificate
+    }
+    this._server = https.createServer(credentials, () => {
+      Logger.log(`HTTPS server listening on port ${this._options?.port}`)
+    })
+  }
+
+  start() {
+    if (!this._application) return
+    if (this._options?.privateKey && this._options?.certificate) {
+      // this.createHttpsServer()
+      this.createHttpServer()
+    } else {
+      this.createHttpServer()
+    }
+  }
+
+  stop() {
+    if (!this._server) return
+    this._server.close(() => {})
+  }
 
   get option() {
-    return this._option
+    return this._options
   }
 
   set option(options: AppServerOptions | undefined) {
-    this._option = options
+    this._options = options
   }
 
   getInstance() {
@@ -40,6 +92,15 @@ export class AppServer {
   }
 }
 
-export function createServer(options: AppServerOptions) {
-  return new AppServer(options)
+export function createAppServer() {
+  const options = {
+    path: "F:\\Project\\Electron-React-Rspack\\app\\public",
+    port: 59098,
+    hostname: "127.0.0.1",
+    isSafe: true
+    // privateKey,
+    // certificate
+  }
+  const appServer = new AppServer(options)
+  appServer.start()
 }
