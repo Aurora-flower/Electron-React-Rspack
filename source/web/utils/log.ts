fd3fd699fe2dfeb94683ed8a126d2bc7e1aca521
花楹一间 <yanl_802@163.com>
2025-05-07 13:54:49 +0800

    feat(web): 添加日志格式和错误记录功能
    - 实现带颜色高亮的格式化 webLog函数
    - 添加 webErrorLog 函数，用于输出错误

--- source/web/utils/log.ts
+++ source/web/utils/log.ts
@@ -1,3 +1,31 @@
+import CommonUtility from "@/utils/utility"
+
+export const LOG_COLOR = {
+  red: "color: red;",
+  green: "color: green;",
+  blue: "color: blue;",
+  yellow: "color: yellow;",
+  orange: "color: orange;",
+  purple: "color: purple;",
+  pink: "color: pink;",
+  cyan: "color: cyan;"
+}
+
 export function webLog(moduleId: string, sign: string, ...args: unknown[]) {
-  console.log(`>>> Web Log Source [ ${moduleId} ] - $_${sign}_$`, ...args)
+  console.log(
+    `%c>>> Web Log Source %c[ ${moduleId} ] - %c$_${sign}_$`,
+    LOG_COLOR.cyan,
+    LOG_COLOR.pink,
+    LOG_COLOR.green,
+    ...args
+  )
+}
+
+export function webErrorLog(moduleId: string, sign: string, error: unknown) {
+  console.error(
+    `>>> Web Error Source %c[ ${moduleId} ] - %c$_${sign}_$`,
+    LOG_COLOR.blue,
+    LOG_COLOR.yellow,
+    CommonUtility.errorMessage(error)
+  )
 }
