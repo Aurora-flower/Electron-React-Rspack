44250b928c2fac185d7fa36b61a971eecfebb32e
花楹一间 <yanl_802@163.com>
2025-05-16 16:17:23 +0800

    feat(common): add tag regex and update utilities
    - Add regex for matching HTML tags
    - Introduce isRGBA and isTag utility functions
    - Update rgba regex to be a property of the regExp object

--- source/web/common/regExp.ts
+++ source/web/common/regExp.ts
@@ -1,3 +1,12 @@
 export const regExp = {
-  rgba: /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})?$/i // 6 位 | 8 位
+  rgba: /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})?$/i, // 6 位 | 8 位
+  tag: /<([a-zA-Z]+)([^>]*)>(.*?)<\/\1>/g
+}
+
+export function isRGBA(color: string): boolean {
+  return regExp.rgba.test(color)
+}
+
+export function isTag(text: string): boolean {
+  return regExp.tag.test(text)
 }
