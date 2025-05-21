b7163a0ad1be99cd8598f093ea329158b2f9421c
花楹一间 <yanl_802@163.com>
2025-05-21 19:46:57 +0800

    feat(utils): add webWarn function and gray log color

--- source/web/utils/log.ts
+++ source/web/utils/log.ts
@@ -8,7 +8,8 @@ export const LOG_COLOR = {
   orange: "color: orange;",
   purple: "color: purple;",
   pink: "color: pink;",
-  cyan: "color: cyan;"
+  cyan: "color: cyan;",
+  gray: "color: gray;"
 }
 
 export function webLog(
@@ -35,5 +36,19 @@ export function webErrorLog(
     LOG_COLOR.blue,
     LOG_COLOR.yellow,
     CommonUtility.errorMessage(error)
+  )
+}
+
+export function webWarn(
+  moduleId: string,
+  sign: string,
+  ...args: unknown[]
+): void {
+  console.warn(
+    `%c>>> Web Warn Source %c[ ${moduleId} ] - %c$_${sign}_$`,
+    LOG_COLOR.yellow,
+    LOG_COLOR.gray,
+    LOG_COLOR.red,
+    ...args
   )
 }
