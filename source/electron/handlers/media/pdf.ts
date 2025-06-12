e9f8c8bb7895c4cfe5f17dc6fbcd6d58bcfad954
花楹一间 <yanl_802@163.com>
2025-06-12 23:12:21 +0800

    refactor(media): 重构 PDF 打印功能为单独的函数,而不是直接依赖 IpcMain

--- source/electron/handlers/media/pdf.ts
+++ source/electron/handlers/media/pdf.ts
@@ -1,20 +1,18 @@
-import { writeFile } from "node:fs"
-import { tmpdir } from "node:os"
-import { join } from "node:path"
-import { getWebContentsWindow } from "@main/features/window"
-import { ipcMain, shell } from "electron"
+import WindowManager from "@main/helpers/manager/window"
+import { sendLog } from "@main/toolkit/logger"
+import type { PrintToPDFOptions } from "electron"
 
-ipcMain.on("print-to-pdf", event => {
-  const pdfPath = join(tmpdir(), "print.pdf")
-  const win = getWebContentsWindow(event.sender)
-  if (!win) return
-  // 使用默认打印参数
-  win.webContents.printToPDF({}, (error, data) => {
-    if (error) throw error
-    writeFile(pdfPath, data, error => {
-      if (error) throw error
-      shell.openExternal(`file://${pdfPath}`)
-      event.sender.send("wrote-pdf", pdfPath)
-    })
+export function printToPDF(pdf: string, options: PrintToPDFOptions = {}): void {
+  const window = WindowManager.getInstance().mainWindow
+  if (!window) return
+  window.webContents.printToPDF(options).then(data => {
+    // TODO: PDF 打印功能
+    sendLog(
+      {
+        sign: "print-to-pdf",
+        module: module.id
+      },
+      data
+    )
   })
-})
+}
