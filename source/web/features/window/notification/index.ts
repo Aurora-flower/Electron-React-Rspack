fb5c1e683d844e4ca1ff4d44e9d079a942943097
花楹一间 <yanl_802@163.com>
2025-06-16 16:06:30 +0800

    feat(ui): 添加通知功能
    - 在 Home 组件中添加了一个 h1 标题
    - 新增 notification 模块，实现高级和简单通知功能
    - 更新路由配置，修正 HOCPage 组件引用

---
+++ source/web/features/window/notification/index.ts
@@ -0,0 +1,20 @@
+// const notification = {
+//   title: "附带图像的通知",
+//   body: "短消息附带自定义图片"
+// }
+// const myNotification = new window.Notification(
+//   "Notification title",
+//   notification
+// )
+
+export function advancedNotification(): void {
+  // 高级通知
+}
+
+export function simpleNotification(): void {
+  // 简单通知 | 基础通知
+  if (!("Notification" in window)) {
+    // 检查浏览器是否支持通知
+    // alert("当前浏览器不支持桌面通知");
+  }
+}
--- source/web/ui/pages/Home/index.tsx
+++ source/web/ui/pages/Home/index.tsx
@@ -3,7 +3,11 @@ import * as React from "react"
 import type { JSX } from "react"
 
 function Home(): JSX.Element {
-  return <div>Home</div>
+  return (
+    <div>
+      <h1>Home</h1>
+    </div>
+  )
 }
 
 export default Home
