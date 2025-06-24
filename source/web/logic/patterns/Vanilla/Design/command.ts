7090592bcc22e0c05bda83b0e4d8f2cbf9c4512c
花楹一间 <yanl_802@163.com>
2025-06-24 16:09:38 +0800

    mod: 命令模式代码示例

--- source/web/logic/patterns/Vanilla/Design/command.ts
+++ source/web/logic/patterns/Vanilla/Design/command.ts
@@ -5,8 +5,10 @@
 
 interface Order {
   id: string
+  name?: string
   status: "processing" | "completed" | "cancelled" | "pending"
 }
+type OrderList = Array<Order>
 
 /**
  * @summary 示例：在线食品配送平台，支持下订单、跟踪和取消订单
@@ -54,12 +56,58 @@ generalOrderManager.cancelOrder(order.id)
 
 /* ***** ***** 命令模式 ***** *****  */
 
-class OrderManager {
-  private orders: Order[]
+type Executor<T> = (orders: T, ...args: unknown[]) => void
 
+class CommandExecutor<T> {
+  execute: Executor<T>
+
+  constructor(execute: Executor<T>) {
+    this.execute = execute
+  }
+}
+
+type OrderCommandExecutor = CommandExecutor<OrderList>
+
+class OrderManager {
+  private orders: OrderList
   constructor() {
     this.orders = []
   }
+
+  executeCommand(command: OrderCommandExecutor, ...args: unknown[]): any {
+    return command.execute(this.orders, ...args)
+  }
 }
+
+// function PlaceOrderCommand(order: Order, id: string): OrderCommandExecutor {
+//   return new CommandExecutor((orders: OrderList) => {
+//     orders.push({ id, status: "pending" })
+//     return `You have successfully ordered ${order} (${id})`
+//   })
+// }
+
+class PlaceOrderCommand implements OrderCommandExecutor {
+  constructor(
+    private order: Order,
+    private id: string
+  ) {}
+
+  execute(orders: OrderList): void {
+    orders.push({ id: this.id, status: "pending" })
+    console.log(`成功下单 ${this.order.name} (${this.id})`)
+  }
+}
+
+const manager = new OrderManager()
+manager.executeCommand(
+  new PlaceOrderCommand(
+    {
+      status: "pending",
+      name: "测试",
+      id: "1234"
+    },
+    "1234"
+  )
+)
 
 export default OrderManager
