340b124aa4b4924d46fccd60c1d1adfbf3e73a5e
花楹一间 <yanl_802@163.com>
2025-07-04 10:40:50 +0800

    feat(document): 增强元素获取功能并优化类型定义
    - 新增通过 name、tag、class 获取元素的功能
    - 优化 ElementGetterWay 类型定义，使用 const enum
    - 添加 FilteredGetterWay 类型，限制特定的获取方式
    - 重构 getDomElement 函数，支持新的获取方式
    - 新增 getDomElements 函数，返回多个元素

--- source/web/features/document/index.ts
+++ source/web/features/document/index.ts
@@ -1,17 +1,43 @@
-type ElementGetterWay = "selector" | "id"
+const GETTER_WAY = {
+  Id: "id",
+  Selector: "selector",
+  Name: "name",
+  Tag: "tag",
+  Class: "class"
+}
+
+type ElementGeretterWayKeys = keyof typeof GETTER_WAY
+
+type ElementGetterWayValues = (typeof GETTER_WAY)[ElementGeretterWayKeys]
+
+type FilteredGetterWay = Omit<typeof GETTER_WAY, "Class" | "Tag" | "Name">
 
 export function getDomElement(
   parameter: string,
-  way: ElementGetterWay = "selector"
+  way: (typeof GETTER_WAY)[keyof FilteredGetterWay]
 ): HTMLElement | null {
-  if (way === "selector") {
+  if (way === GETTER_WAY.Selector) {
     return document.querySelector(parameter) as HTMLElement
-  } else if (way === "id") {
+  } else if (way === GETTER_WAY.Id) {
     return document.getElementById(parameter) as HTMLElement
   }
   return null
 }
 
-export function getRootElement(root: string | HTMLElement): HTMLElement {
-  return typeof root === "string" ? (getDomElement(root) as HTMLElement) : root
+export function getDomElements(
+  parameter: string,
+  way: Exclude<ElementGetterWayValues, "id">
+): Array<Element> {
+  // 统一返回真数组
+  if (way === GETTER_WAY.Name) {
+    return Array.from(document.getElementsByName(parameter))
+  } else if (way === GETTER_WAY.Tag) {
+    return Array.from(document.getElementsByTagName(parameter))
+  } else if (way === GETTER_WAY.Class) {
+    return Array.from(document.getElementsByClassName(parameter))
+  } else if (way === GETTER_WAY.Selector) {
+    return Array.from(document.querySelectorAll(parameter))
+  } else {
+    return []
+  }
 }
