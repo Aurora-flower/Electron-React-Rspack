2e0bd8f12c632577271dac6c9e4f307d81f3ad42
花楹一间 <yanl_802@163.com>
2025-07-07 00:41:42 +0800

    fix(grid): 修复单例模式获取逻辑，并添加释放功能

--- source/web/helpers/graphics/gremlin/controller/assistant/grid.ts
+++ source/web/helpers/graphics/gremlin/controller/assistant/grid.ts
@@ -2,25 +2,29 @@ import { DEFAULT_GRID_INTERVAL } from "@/helpers/graphics/gremlin/constant/defau
 import { viewAppend } from "@/helpers/graphics/gremlin/functions/append"
 import { drawLine } from "@/helpers/graphics/gremlin/generator/graphics/drawLine"
 import { getPoint } from "@/utils/functions/usually"
-import type { Container, StrokeInput } from "pixi.js"
+import type { Container, DestroyOptions, StrokeInput } from "pixi.js"
 import { Graphics } from "pixi.js"
 
 class Grid {
   private static _instance: Grid
-  private _grid: Graphics = new Graphics()
+  _grid: Graphics = new Graphics({
+    // label: Date.now().toString()
+  })
+
   // constructor(root: Container) {
   // }
 
   public static getInstance(): Grid {
-    if (Grid._instance) {
-      return Grid._instance
+    if (!Grid._instance) {
+      Grid._instance = new Grid()
     }
-    return new Grid()
+    return Grid._instance
   }
 
   draw(
     parent: Container,
-    size?: SizeModel,
+    size: SizeModel,
+    scale = 1,
     strokeInput?: StrokeInput,
     girdInterval?: PointModel
   ): void {
@@ -29,19 +33,35 @@ class Grid {
       height: 0
     }
     const renderInterval =
-      girdInterval ?? getPoint(DEFAULT_GRID_INTERVAL, DEFAULT_GRID_INTERVAL)
+      girdInterval ??
+      getPoint(DEFAULT_GRID_INTERVAL * scale, DEFAULT_GRID_INTERVAL * scale)
     const style = strokeInput ?? {
       width: 1,
       color: 0xcccccc,
       alpha: 0.3
     }
     this.gridLogic(renderSize, renderInterval, style)
-    viewAppend(parent, [this._grid])
+    if (!parent.children.includes(this._grid)) {
+      viewAppend(parent, [this._grid])
+    }
   }
 
-  clear(): void {}
-
-  destory(): void {}
+  /**
+   *
+   * @param isClean
+   * @param configuration
+   * 参数名	类型	作用	默认值（不传时）
+   */
+  release(
+    isClean = false,
+    configuration?: DestroyOptions /*DEFAULT_DESTORY_OPTIONS */
+  ): void {
+    if (isClean) {
+      this._grid.destroy(configuration)
+    } else {
+      this._grid.clear()
+    }
+  }
 
   private gridLogic(
     size: SizeModel,
@@ -53,18 +73,15 @@ class Grid {
     if (size.width <= gridInterval.x || size.height <= gridInterval.y) {
       return
     }
-
     for (let x = 0; x <= size.width; x += gridInterval.x) {
       const startPoint: [number, number] = [x, 0]
       const endPoint: [number, number] = [x, size.height]
       drawLine([startPoint, endPoint], this._grid, style)
-      console.log([[startPoint, endPoint]])
     }
     for (let y = 0; y <= size.height; y += gridInterval.y) {
       const startPoint: [number, number] = [0, y]
       const endPoint: [number, number] = [size.width, y]
       drawLine([startPoint, endPoint], this._grid, style)
-      console.log([[startPoint, endPoint]])
     }
   }
 }
