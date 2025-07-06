76a6ac502af0cf8d3ebff934eb2b19f279f78ba4
花楹一间 <yanl_802@163.com>
2025-07-07 00:50:14 +0800

    feat(menus): 添加刷新功能并注释相关菜单项
    - 在应用菜单中添加了"刷新"选项，快捷键为 CmdOrCtrl+R
    - 注释了"强制刷新"和"退出"菜单项的代码
    - 优化了"打开调试工具"菜单项的代码结构

--- source/electron/features/menus/applicationMenu.ts
+++ source/electron/features/menus/applicationMenu.ts
@@ -48,13 +48,54 @@ export function registerAppMenu(): void {
           accelerator: "CmdOrCtrl+Shift+I",
           click: (menuItem, window: BrowserWindow): void => {
             window.webContents.openDevTools()
+            // win.webContents.openDevTools({
+            //   mode: "detach",
+            //   activate: true
+            // })
             sendLog({
               module: module?.id,
               sign: "打开调试工具",
               level: "info"
             })
           }
+        },
+        {
+          label: "刷新",
+          accelerator: "CmdOrCtrl+R",
+          click: (menuItem, window: BrowserWindow): void => {
+            window.reload()
+            sendLog({
+              module: module?.id,
+              sign: "刷新",
+              level: "info"
+            })
+          }
         }
+        // {
+        //   label: "强制刷新",
+        //   accelerator: "CmdOrCtrl+Shift+R",
+        //   click: (menuItem, window: BrowserWindow): void => {
+        //     window.reload()
+        //     window.webContents.reloadIgnoringCache()
+        //     sendLog({
+        //       module: module?.id,
+        //       sign: "强制刷新",
+        //       level: "info"
+        //     })
+        //   }
+        // }
+        // {
+        //   label: "退出",
+        //   accelerator: "CmdOrCtrl+Q",
+        //   click: (): void => {
+        //     app.quit()
+        //     sendLog({
+        //       module: module?.id,
+        //       sign: "退出",
+        //       level: "info"
+        //     })
+        //   }
+        // }
       ]
     })
   )
