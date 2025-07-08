c303d5573279b849a36f971eba5fcc38ccb4820f
花楹一间 <yanl_802@163.com>
2025-07-09 01:22:09 +0800

    chore: 初始化 rspack 配置文件
    - 新增 rspack.config.ts 文件
    - 添加基本的 RSpack 配置结构
    - 注释掉一些可选的导入和配置项，留待后续根据需要启用

---
+++ rspack.config.ts
@@ -0,0 +1,10 @@
+import { defineConfig } from "@rspack/cli"
+// import { rspack } from "@rspack/core";
+// import { ReactRefreshRspackPlugin } from "@rspack/plugin-react-refresh";
+
+// const isDev = process.env.NODE_ENV === "development";
+
+// Target browsers, see: https://github.com/browserslist/browserslist
+// const targets = ["last 2 versions", "> 0.2%", "not dead", "Firefox ESR"];
+
+export default defineConfig([])
