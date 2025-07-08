e859a178373f79012bcf49cb92624d5afec8ec23
花楹一间 <yanl_802@163.com>
2025-07-09 01:38:46 +0800

    build: 添加 rspack 配置并扩展 package.json 脚本
    - 在 package.json 中添加 debug:dev 和 debug:build 脚本
    - 创建 rspack.config.ts 文件，配置 rspack 开发环境
    - 设置 electron-main、electron-preload 和 web 三个目标
    - 配置源码映射、别名和输出路径
    - 启用 CSS 实验性功能和详细日志记录

--- rspack.config.ts
+++ rspack.config.ts
@@ -7,4 +7,94 @@ import { defineConfig } from "@rspack/cli"
 // Target browsers, see: https://github.com/browserslist/browserslist
 // const targets = ["last 2 versions", "> 0.2%", "not dead", "Firefox ESR"];
 
-export default defineConfig([])
+export default defineConfig([
+  {
+    "mode": "development",
+    "stats": "verbose",
+    "devtool": "source-map",
+    "resolve": {
+      "mainFiles": ["index", "main"],
+      "extensions": ["...", ".ts", ".js", ".json", ".node"],
+      "enforceExtension": false,
+      "symlinks": false,
+      "alias": {
+        "@main": "/Users/HuaYing/Desktop/resources/Local/ER/source/electron"
+      }
+    },
+    "target": "electron-main",
+    "entry": {
+      "main": {
+        "import":
+          "/Users/HuaYing/Desktop/resources/Local/ER/source/electron/main.ts"
+      }
+    },
+    "output": {
+      "path": "/Users/HuaYing/Desktop/resources/Local/ER/app/electron",
+      "filename": "index.js",
+      "clean": true
+    },
+    "module": {},
+    "plugins": []
+  },
+  {
+    "mode": "development",
+    "stats": "verbose",
+    "devtool": "source-map",
+    "resolve": {
+      "mainFiles": ["index", "main"],
+      "extensions": ["...", ".ts", ".js", ".json", ".node"],
+      "enforceExtension": false,
+      "symlinks": false
+    },
+
+    "target": "electron-preload",
+    "entry": {
+      "preload": {
+        "import":
+          "/Users/HuaYing/Desktop/resources/Local/ER/source/preload/index.ts"
+      }
+    },
+    "output": {
+      "path": "/Users/HuaYing/Desktop/resources/Local/ER/app/preload",
+      "filename": "index.js",
+      "clean": true
+    },
+    "module": {},
+    "plugins": []
+  },
+  {
+    "mode": "development",
+    "stats": "verbose",
+    "devtool": "source-map",
+    "resolve": {
+      "mainFiles": ["index", "main"],
+      "extensions": ["...", ".ts", ".js", ".json", ".node", ".tsx", ".css"],
+      "enforceExtension": false,
+      "symlinks": false,
+      "alias": {
+        "@": "/Users/HuaYing/Desktop/resources/Local/ER/source/web"
+      }
+    },
+    "target": "web",
+    "entry": {
+      "renderer": {
+        "import": "/Users/HuaYing/Desktop/resources/Local/ER/source/web/main.ts"
+      }
+    },
+    "output": {
+      "path": "/Users/HuaYing/Desktop/resources/Local/ER/app/public",
+      "filename": "index.js",
+      "clean": true,
+      "publicPath": "/"
+    },
+    "experiments": {
+      "css": true
+    },
+    "infrastructureLogging": {
+      "level": "verbose"
+    },
+    "optimization": {},
+    "module": {},
+    "plugins": []
+  }
+])
