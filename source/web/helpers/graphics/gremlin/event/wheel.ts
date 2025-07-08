097bbdd217f6d895cedc39347576ab64d5c6eed5
花楹一间 <yanl_802@163.com>
2025-07-08 10:12:10 +0800

    perf(wheel): 优化 wheel 事件处理性能
    - 引入 CommonUtility.throttle 函数来节流 wheel 事件处理
    - 通过 throttle 函数限制事件处理频率，提高性能
    - 调整了事件处理函数的参数接收方式，提高代码可读性

--- source/web/helpers/graphics/gremlin/event/wheel.ts
+++ source/web/helpers/graphics/gremlin/event/wheel.ts
@@ -1,13 +1,16 @@
 import { CURSOR } from "@/common/cursor"
 import PixiManager from "@/helpers/graphics/gremlin"
 import { formatNumberPrecision } from "@/utils/functions/math"
+import CommonUtility from "@/utils/utility"
 import type { Container, FederatedWheelEvent } from "pixi.js"
 
 const ZOOM_SPEED = 0.05
 // const SCALE_RATIO = 0.1;
 
 export function addStageWheel(stage: Container): void {
-  const wheelHandler = (e: FederatedWheelEvent): void => {
+  const wheelHandler = CommonUtility.throttle((ev): void => {
+    const e = ev as FederatedWheelEvent
+    // requestAnimationFrame(() => {
     e.preventDefault()
     e.stopPropagation()
     stage.cursor = e.deltaY > 0 ? CURSOR.Out : CURSOR.In
@@ -17,9 +20,9 @@ export function addStageWheel(stage: Container): void {
       delta > 0 ? canvasScale - ZOOM_SPEED : canvasScale + ZOOM_SPEED
     )
     PixiManager.setDrawingBoardScale(zoomFactor)
+    // })
     // const mousePosition = e.global.clone() // 获取鼠标在画布上的位置（缩放中心点）
     // PixiManager.setZoom(zoomFactor, mousePosition)
-  }
-
+  }, 300)
   stage.on("wheel", wheelHandler)
 }
