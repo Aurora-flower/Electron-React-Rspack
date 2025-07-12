d59db474a989bf8f734e8be1c970b52d22f87501
花楹一间 <yanl_802@163.com>
2025-07-12 22:07:59 +0800

    refactor(web): 优化选择器边框绘制精度
    - 引入 formatNumberPrecision 和 getSize 函数，提高计算精度
    - 调整边框偏移量，确保边框宽度均匀
    - 优化尺寸和位置计算逻辑，提高选择器显示效果

--- source/web/helpers/graphics/gremlin/controller/selector/index.ts
+++ source/web/helpers/graphics/gremlin/controller/selector/index.ts
@@ -3,6 +3,8 @@ import { Graphics } from "pixi.js"
 import { ELEMENT_FLAG } from "@/helpers/graphics/gremlin/constant/elementFlag"
 import { viewAppend } from "@/helpers/graphics/gremlin/functions/append"
 import { getCumulativeScale } from "@/helpers/graphics/gremlin/functions/compute"
+import { formatNumberPrecision } from "@/utils/functions/math"
+import { getSize } from "@/utils/functions/usually"
 
 const BORAD_WIDTH = 3
 
@@ -10,8 +12,8 @@ const BORAD_COLOR = 0x3d80a5
 
 const STORKE_STYLE = {
   width: BORAD_WIDTH,
-  color: BORAD_COLOR,
-  alpha: 0.5
+  color: BORAD_COLOR
+  // alpha: 0.5
 }
 
 class Selector {
@@ -48,18 +50,19 @@ class Selector {
       x: getCumulativeScale(ancestorContainer, "x"),
       y: getCumulativeScale(ancestorContainer, "y")
     }
-    const size = {
-      width: target.width * scale.x * targetContainer.scale.x,
-      height: target.height * scale.y * targetContainer.scale.y
-    }
+    const borderOffset = BORAD_WIDTH / 2
+    const size = getSize(
+      target.width * scale.x * targetContainer.scale.x - borderOffset,
+      target.height * scale.y * targetContainer.scale.y - borderOffset
+    )
     const pos = target.getGlobalPosition().clone()
     Selector.move(pos)
     Selector._selector.pivot.set(
-      (size.width * 0.5) / scale.x - BORAD_WIDTH,
-      (size.height * 0.5) / scale.y - BORAD_WIDTH
+      formatNumberPrecision((size.width * 0.5) / scale.x - BORAD_WIDTH),
+      formatNumberPrecision((size.height * 0.5) / scale.y - BORAD_WIDTH)
     )
     Selector._selector
-      .rect(BORAD_WIDTH, BORAD_WIDTH, size.width, size.height)
+      .rect(borderOffset, borderOffset, size.width, size.height)
       .stroke(STORKE_STYLE)
   }
 
