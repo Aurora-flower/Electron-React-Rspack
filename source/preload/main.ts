408eee536b608d5c73714688393eaed364fcf6af
花楹一间 <yanl_802@163.com>
2025-07-15 18:48:54 +0800

    test: 测试当多个 preload 模块文件时是否能正常编译运行

---
+++ source/preload/main.ts
@@ -0,0 +1,22 @@
+import type { IpcRendererEvent } from "electron"
+import { contextBridge, ipcRenderer } from "electron"
+
+// const windowLoaded = new Promise(resolve => {
+//   window.onload = resolve
+// })
+
+contextBridge.exposeInMainWorld("IPC", {
+  dispatch: (...args: ArrayType) => ipcRenderer.invoke("dispatch", ...args),
+  emitter: (...args: ArrayType) => ipcRenderer.send("emitter", ...args),
+  sender: (...args: ArrayType) => ipcRenderer.send("sender", ...args)
+})
+
+ipcRenderer.on(
+  "message-transmit",
+  (_event: IpcRendererEvent, message: MessageEventInit) => {
+    const replay: MessageEventInit<MessageEventInit<unknown> | undefined> = {
+      data: message
+    }
+    window.dispatchEvent(new MessageEvent("message", replay))
+  }
+)
