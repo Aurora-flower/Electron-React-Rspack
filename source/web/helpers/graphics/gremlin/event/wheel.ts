a108ff1c07e76bd641bead08662eaced8557c273
花楹一间 <yanl_802@163.com>
2025-07-17 18:55:36 +0800

    mod: 暂时屏蔽掉以鼠标位置为中心点的缩放

--- source/web/helpers/graphics/gremlin/event/wheel.ts
+++ source/web/helpers/graphics/gremlin/event/wheel.ts
@@ -1,7 +1,7 @@
 import type { Container, FederatedWheelEvent } from "pixi.js"
 import PixiManager from "@/helpers/graphics/gremlin"
-import { ELEMENT_FLAG } from "@/helpers/graphics/gremlin/constant/elementFlag"
-import { getElementByLabel } from "@/helpers/graphics/gremlin/functions/filter"
+// import { ELEMENT_FLAG } from "@/helpers/graphics/gremlin/constant/elementFlag"
+// import { getElementByLabel } from "@/helpers/graphics/gremlin/functions/filter"
 import { roundToDecimal } from "@/utils/functions/math"
 
 const ZOOM_SPEED = 0.05
@@ -21,16 +21,16 @@ export function addStageWheel(stage: Container): void {
         delta > 0 ? canvasScale - ZOOM_SPEED : canvasScale + ZOOM_SPEED
       )
       // TODO: 以鼠标为中心缩放
-      if (PixiManager.isVaildScale(scale)) {
-        const zoomFactor = roundToDecimal(PixiManager.viewScale - scale, 2)
-        const root = getElementByLabel(ELEMENT_FLAG.Root, stage)
-        if (root) {
-          // const mousePosition = e.global.clone() // 获取鼠标在画布上的位置（缩放中心点）
-          const mouseLocal = e.getLocalPosition(root)
-          root.position.x += mouseLocal.x * zoomFactor
-          root.position.y += mouseLocal.y * zoomFactor
-        }
-      }
+      // if (PixiManager.isVaildScale(scale)) {
+      //   const zoomFactor = roundToDecimal(PixiManager.viewScale - scale, 2)
+      //   const root = getElementByLabel(ELEMENT_FLAG.Root, stage)
+      //   if (root) {
+      //     // const mousePosition = e.global.clone() // 获取鼠标在画布上的位置（缩放中心点）
+      //     const mouseLocal = e.getLocalPosition(root)
+      //     root.position.x += mouseLocal.x * zoomFactor
+      //     root.position.y += mouseLocal.y * zoomFactor
+      //   }
+      // }
 
       PixiManager.setDrawingBoardScale(scale)
     }
