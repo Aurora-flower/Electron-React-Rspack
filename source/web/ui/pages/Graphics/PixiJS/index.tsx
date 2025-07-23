269ff4f8440768a63a2781c5145a7c5eb41a50b6
花楹一间 <yanl_802@163.com>
2025-07-23 17:50:45 +0800

    refactor(web/ui): 恢复 PixiJS 图形页面的提示信息
    - 启用了之前被注释掉的提示信息组件
    - 添加了 pointer-event-none 类，确保提示信息不会干扰鼠标事件

--- source/web/ui/pages/Graphics/PixiJS/index.tsx
+++ source/web/ui/pages/Graphics/PixiJS/index.tsx
@@ -5,9 +5,9 @@ import PixiManager from "@/helpers/graphics/gremlin"
 import { DEFAULT_DESTROY_OPTIONS } from "@/helpers/graphics/gremlin/options/destroyOptions"
 
 function PixiJsGraphics(): JSX.Element {
-  // const [getTip] = React.useState<string>(
-  //   "使用鼠标右键平移视窗焦点，使用滚轮缩放视图"
-  // )
+  const [getTip] = React.useState<string>(
+    "使用鼠标右键平移视窗焦点，使用滚轮缩放视图"
+  )
   const appRef = React.useRef<Application>(null)
   const renderBoxRef = React.useRef<HTMLDivElement>(null)
 
@@ -32,7 +32,9 @@ function PixiJsGraphics(): JSX.Element {
 
   return (
     <div className="w-full h-full">
-      {/* <div className="tip absolute top-6 left-6 opacity-80">{getTip}</div> */}
+      <div className="tip pointer-event-none absolute top-6 left-6 opacity-80">
+        {getTip}
+      </div>
       <div ref={renderBoxRef} className="w-full h-full" id="graphics" />
     </div>
   )
